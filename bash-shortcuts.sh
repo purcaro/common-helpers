b195722bd9224e1091cdf4b038d2c491e7827ef0
Michael Purcaro <purcaro@gmail.com>
2026-05-24 11:37:37 -0400

    more aliases

--- bash-shortcuts.sh
+++ bash-shortcuts.sh
@@ -44,6 +44,7 @@ alias qmv='qmv --format=destination-only'
 alias dlmp3='yt-dlp -x --extract-audio --audio-format mp3'
 alias   c="${_COMMON_HELPERS}/count_files.sh"
 alias   d='du -hc -d 1 | sort -h'
+alias  sq="sudo EDITOR='emacs -nw' qmv --format=destination-only"
 
 # make clean (separate from c=count_files.sh)
 alias mc='make clean'
